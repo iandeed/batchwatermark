889e51638bbaae0c60e841394274d508a324bede
iandeed <gooddeed89@gmail.com>
2018-03-24 11:20:12 +0000

    finished resizing instruction and tidied output

--- batch_watermark.py
+++ batch_watermark.py
@@ -3,7 +3,6 @@
 
 import os
 import argparse
-import time
 
 def main():
     parser = argparse.ArgumentParser(description='Add watermarks to images in path')
@@ -12,14 +11,13 @@ def main():
     parser.add_argument('--name', help='Name addition for watermark', default="-watermark", type=str)
     parser.add_argument('--extension', help='Image extensions to look for', default=".jpg", type=str)
     parser.add_argument('--position', help='Cardinal position to place the watermark', default="center", type=str)
-    parser.add_argument('--resize', help='Percentage by which to resize the image', default="100", type str)
+    parser.add_argument('--resize', help='Percentage by which to resize the image', default="100", type=str)
     parser.add_argument('--exclude', help='Path content to exclude', type=str)
 
     args = parser.parse_args()
 
     files_processed = 0
     files_watermarked = 0
-    start_time = time.time()
     for dirName, subdirList, fileList in os.walk(args.root):
         if args.exclude is not None and args.exclude in dirName:
             continue
@@ -35,14 +33,13 @@ def main():
                     files_watermarked += 1
                     print('    Convert %s to %s' % (orig, new_name))
                     os.system('composite -dissolve 70%% -gravity %s %s "%s" "%s"' % (args.position, args.watermark, orig, new_name))
-                    if args.resize < 100:
+                    if int(args.resize) < 100:
                         #Scale down the image
-                        os.system('convert -resize %s%% %s %s', args.resize, new_name, new_name)
+                        print('resizing to %s%% of original' % (args.resize))
+                        os.system('convert -resize %d %s %s' % (int(args.resize), new_name, new_name))
 
     print("Files Processed: %s" % "{:,}".format(files_processed))
     print("Files Watermarked: %s" % "{:,}".format(files_watermarked))
-    elapsed_time = time.Time() - start_time
-    print("Time taken: %s", elapsed_time)
 
 if __name__ == '__main__':
     main()
